10c4ce555afe77820f3680f3b19503314aa9f2a0
Giorgi <giorgiamirkhanashvili@gmail.com>
2025-05-10 22:04:34 +0100

    Big change to editablelabel to get the keyboard to come up on first tap.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "289760785"
+    serviceWorkerVersion: "3663027869"
   }
 });
