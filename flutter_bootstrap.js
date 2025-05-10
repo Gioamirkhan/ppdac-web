d6f4d06a603e1e1a3051adf3e9e0a568801d0a31
Giorgi <giorgiamirkhanashvili@gmail.com>
2025-05-10 22:16:02 +0100

    Reverting to the version where editablelable changes worked well but keyboard does not come up at the fist tap.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1048329190"
+    serviceWorkerVersion: "3078904377"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1464bab2278e7ad03d8e2b0b02c5ced1",
+const RESOURCES = {"flutter_bootstrap.js": "756480db489910955fb9a55ec2a64cdc",
 "version.json": "55067a20a55fb044c02a90028a674666",
 "index.html": "849697bd44685f68623797165a230098",
 "/": "849697bd44685f68623797165a230098",
-"main.dart.js": "152672542f06da92107cbec0dea74101",
+"main.dart.js": "76c50aef6a9c143ea51118f6f659d01f",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
